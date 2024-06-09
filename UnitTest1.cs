08c9f256cde4898cc5fdb24e018e3a653eb381b3
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-06-09 16:07:45 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Eighteenth_lesson
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ WebDrivers/Driver.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eighteenth_lesson.WebDrivers
+{
+    internal class Driver
+    {
+        private static IWebDriver _driver;
+
+        public static IWebDriver GetDriver()
+        {
+            if(_driver == null)
+            {
+                return new ChromeDriver(GetOptions());
+            }
+            return _driver;
+        }
+
+        private static ChromeOptions GetOptions()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            return options;
+        }
+    }
+}
